f0021f0c2436a94fc4cb2562806908a4f9f5815f
Jerrad Thramer <jerrad.thramer@mapbox.com>
2018-01-04 21:00:37 -0700

    Changing example to be a more arbitrary representation.

--- Navigation Examples/Examples/EmbeddedExample.swift
+++ Navigation Examples/Examples/EmbeddedExample.swift
@@ -5,31 +5,45 @@ import MapboxDirections
 
 class EmbeddedExampleViewController: UIViewController  {
  
+    @IBOutlet weak var loadButton: UIButton!
+    @IBOutlet weak var container: UIView!
     var route: Route?
-    @IBAction func beginPressed(_ sender: Any) {
-        start()
-    }
-    func start() {
+
+    lazy var options: NavigationRouteOptions = {
         let origin = CLLocationCoordinate2DMake(37.77440680146262, -122.43539772352648)
         let destination = CLLocationCoordinate2DMake(37.76556957793795, -122.42409811526268)
-        let options = NavigationRouteOptions(coordinates: [origin, destination])
-        
+        return NavigationRouteOptions(coordinates: [origin, destination])
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculateDirections()
+    }
+    
+    func calculateDirections() {
         Directions.shared.calculate(options) { (waypoints, routes, error) in
             guard let route = routes?.first, error == nil else {
                 print(error!.localizedDescription)
                 return
             }
-            let nav = NavigationViewController(for: route)
-            nav.title = "Navigation"
-            let somethingElse = UIViewController()
-            somethingElse.view.backgroundColor = .red
-            somethingElse.title = "Something Else"
-            let tab = UITabBarController()
-            tab.title = "Yay, Embedded NVC!"
-            tab.viewControllers = [nav, somethingElse]
-            
-            self.navigationController?.pushViewController(tab, animated: true)
+            self.route = route
+            self.loadButton.isEnabled = true
         }
+    }
+    
+    @IBAction func startEmbeddedNavigation(_ sender: Any) {
+        let nav = NavigationViewController(for: route!)
+        addChildViewController(nav)
+        container.addSubview(nav.view)
+        nav.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            nav.view.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 0),
+            nav.view.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: 0),
+            nav.view.topAnchor.constraint(equalTo: container.topAnchor, constant: 0),
+            nav.view.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: 0)
+            ])
+        self.didMove(toParentViewController: self)
     }
 
 }
+
