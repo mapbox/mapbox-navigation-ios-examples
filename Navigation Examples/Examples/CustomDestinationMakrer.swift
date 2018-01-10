a9fc1700e401794aaf0817efe8ba7381d3ecc7e6
Bobby Sudekum <bobby@mapbox.com>
2018-01-10 14:57:05 -0800

    Delete CustomDestinationMakrer.swift

--- Navigation Examples/Examples/CustomDestinationMakrer.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CustomDestinationMakrer.swift
-//  Navigation-Examples
-//
-//  Created by Bobby Sudekum on 12/19/17.
-//  Copyright © 2017 Mapbox. All rights reserved.
-//
-
-import Foundation
