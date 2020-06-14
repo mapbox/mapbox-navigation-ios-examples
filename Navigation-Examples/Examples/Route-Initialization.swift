323c2543c8faeec2b508b1087f86b5d2534d572e
Maxim Makhun <maxim.makhun@mapbox.com>
2020-06-14 14:43:45 -0700

    Add functionality to create Route via initializers.

--- Navigation-Examples/Examples/Route-Initialization.swift
+++ Navigation-Examples/Examples/Route-Initialization.swift
@@ -13,23 +13,41 @@ class RouteInitializationViewController: UIViewController {
         
         let origin: CLLocationCoordinate2D = CLLocationCoordinate2DMake(37.776818, -122.399076)
         let destination: CLLocationCoordinate2D = CLLocationCoordinate2DMake(37.777407, -122.399814)
-        let distance: CLLocationDistance = 92.200000000000003
-        let expectedTravelTime: TimeInterval = 24.699999999999999
+        let distance: CLLocationDistance = 92.2
+        let expectedTravelTime: TimeInterval = 24.6
         let routeCoordinates = [origin, destination]
-        let departureStep = createRouteStep(text: "You will arrive",
+        let distanceAlongArrivalStep = 18.7
+        let departureSpokenInstruction = SpokenInstruction(distanceAlongStep: distance,
+                                                           text: "Head northwest on 5th Street, then you have arrived at your destination",
+                                                           ssmlText: "")
+        let arrivalSpokenInstruction = SpokenInstruction(distanceAlongStep: distanceAlongArrivalStep,
+                                                         text: "You have arrived at your destination",
+                                                         ssmlText: "")
+        
+        let departureVisualInstructionBanner = createVisualInstructionBanner(text: "You will arrive",
+                                                                             distanceAlongStep: distance)
+        let arrivalVisualInstructionBanner = createVisualInstructionBanner(text: "You have arrived",
+                                                                           distanceAlongStep: distanceAlongArrivalStep)
+        
+        let departureStep = createRouteStep(maneuverLocation: origin,
+                                            maneuverType: .depart,
                                             instructions: "Head northwest on 5th Street",
+                                            initialHeading: 316.0,
+                                            finalHeading: 0.0,
                                             distance: distance,
                                             expectedTravelTime: expectedTravelTime,
-                                            maneuverType: .depart,
-                                            maneuverLocation: origin,
-                                            routeCoordinates: routeCoordinates)
+                                            instructionsSpokenAlongStep: [departureSpokenInstruction, arrivalSpokenInstruction],
+                                            instructionsDisplayedAlongStep: [departureVisualInstructionBanner, arrivalVisualInstructionBanner])
+        departureStep.shape = LineString(routeCoordinates)
         
-        let arrivalStep = createRouteStep(text: "You have arrived",
-                                          instructions: "You have arrived at your destination",
-                                          distance: 0,
-                                          expectedTravelTime: 0,
+        let arrivalStep = createRouteStep(maneuverLocation: destination,
                                           maneuverType: .arrive,
-                                          maneuverLocation: destination)
+                                          instructions: "You have arrived at your destination",
+                                          initialHeading: 0.0,
+                                          finalHeading: 315.0,
+                                          distance: 0,
+                                          expectedTravelTime: 0)
+        arrivalStep.shape = LineString([destination])
         
         let routeLeg = RouteLeg(steps: [departureStep, arrivalStep],
                                 name: "5th Street",
@@ -39,6 +57,19 @@ class RouteInitializationViewController: UIViewController {
         let routeOptions = NavigationRouteOptions(coordinates: routeCoordinates)
         routeLeg.source = routeOptions.waypoints[0]
         routeLeg.destination = routeOptions.waypoints[1]
+        routeLeg.segmentCongestionLevels = [
+            .heavy,
+            .low,
+            .low,
+            .moderate
+        ]
+        
+        routeLeg.expectedSegmentTravelTimes = [
+            20.6,
+            2,
+            1.1,
+            0.9
+        ]
         
         let route = Route(legs: [routeLeg], shape: LineString(routeCoordinates), distance: distance, expectedTravelTime: expectedTravelTime)
         let navigationService = MapboxNavigationService(route: route, routeOptions: routeOptions, simulating: simulationIsEnabled ? .always : .onPoorGPS)
@@ -50,33 +81,60 @@ class RouteInitializationViewController: UIViewController {
     
     // MARK: - Utility methods
     
-    func createRouteStep(text: String,
-                         instructions: String,
-                         distance: CLLocationDistance,
-                         expectedTravelTime: TimeInterval,
-                         maneuverType: ManeuverType,
-                         maneuverLocation: CLLocationCoordinate2D,
-                         routeCoordinates: [CLLocationCoordinate2D]? = nil) -> RouteStep {
-        let component = VisualInstruction.Component.text(text: .init(text: text, abbreviation: nil, abbreviationPriority: nil))
-        let visualInstruction = VisualInstruction(text: text, maneuverType: maneuverType, maneuverDirection: .straightAhead, components: [component])
-        let visualInstructionBanner = VisualInstructionBanner(distanceAlongStep: distance,
+    private func createRouteStep(maneuverLocation: CLLocationCoordinate2D,
+                                 maneuverType: ManeuverType,
+                                 instructions: String,
+                                 initialHeading: CLLocationDirection,
+                                 finalHeading: CLLocationDirection,
+                                 distance: CLLocationDistance,
+                                 expectedTravelTime: TimeInterval,
+                                 instructionsSpokenAlongStep: [SpokenInstruction]? = nil,
+                                 instructionsDisplayedAlongStep: [VisualInstructionBanner]? = nil) -> RouteStep {
+        
+        let routeStep = RouteStep(transportType: .automobile,
+                                  maneuverLocation: maneuverLocation,
+                                  maneuverType: maneuverType,
+                                  maneuverDirection: .straightAhead,
+                                  instructions: instructions,
+                                  initialHeading: initialHeading,
+                                  finalHeading: finalHeading,
+                                  drivingSide: .right,
+                                  exitCodes: [],
+                                  exitNames: [],
+                                  phoneticExitNames: [],
+                                  distance: distance,
+                                  expectedTravelTime: expectedTravelTime,
+                                  names: [],
+                                  phoneticNames: [],
+                                  codes: [],
+                                  destinationCodes: [],
+                                  destinations: [],
+                                  intersections: [],
+                                  speedLimitSignStandard: .mutcd,
+                                  speedLimitUnit: .milesPerHour,
+                                  instructionsSpokenAlongStep: instructionsSpokenAlongStep,
+                                  instructionsDisplayedAlongStep: instructionsDisplayedAlongStep)
+        
+        return routeStep
+    }
+    
+    private func createVisualInstructionBanner(text: String, distanceAlongStep: CLLocationDistance) -> VisualInstructionBanner {
+        
+        let component = VisualInstruction.Component.text(text: .init(text: text,
+                                                                     abbreviation: nil,
+                                                                     abbreviationPriority: nil))
+        
+        let visualInstruction = VisualInstruction(text: text,
+                                                  maneuverType: .arrive,
+                                                  maneuverDirection: .straightAhead,
+                                                  components: [component])
+        
+        let visualInstructionBanner = VisualInstructionBanner(distanceAlongStep: distanceAlongStep,
                                                               primary: visualInstruction,
                                                               secondary: nil,
                                                               tertiary: nil,
                                                               drivingSide: .right)
-        let routeStep = RouteStep(transportType: .automobile,
-                                  maneuverLocation: maneuverLocation,
-                                  maneuverType: maneuverType,
-                                  instructions: instructions,
-                                  drivingSide: .right,
-                                  distance: distance,
-                                  expectedTravelTime: expectedTravelTime,
-                                  instructionsDisplayedAlongStep: [visualInstructionBanner])
         
-        if maneuverType == .depart, let routeCoordinates = routeCoordinates {
-            routeStep.shape = LineString(routeCoordinates)
-        }
-        
-        return routeStep
+        return visualInstructionBanner
     }
 }
