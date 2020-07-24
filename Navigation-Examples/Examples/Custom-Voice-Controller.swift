2518614d034c80b74304cc3084eede23aa2b31b2
udumft <viktor.kononov@mapbox.com>
2020-07-24 17:08:37 +0300

    vk-voice-example: updated Custom Voice Controller example with latest API chages.

--- Navigation-Examples/Examples/Custom-Voice-Controller.swift
+++ Navigation-Examples/Examples/Custom-Voice-Controller.swift
@@ -3,15 +3,13 @@ import UIKit
 import MapboxCoreNavigation
 import MapboxNavigation
 import MapboxDirections
+import MapboxSpeech
 import AVFoundation
 
 class CustomVoiceControllerUI: UIViewController {
     
-    var voiceController: CustomVoiceController?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         let origin = CLLocationCoordinate2DMake(37.77440680146262, -122.43539772352648)
         let destination = CLLocationCoordinate2DMake(37.76556957793795, -122.42409811526268)
@@ -28,8 +26,14 @@ class CustomVoiceControllerUI: UIViewController {
                 
                 // For demonstration purposes, simulate locations if the Simulate Navigation option is on.
                 let navigationService = MapboxNavigationService(route: route, routeOptions: routeOptions, simulating: simulationIsEnabled ? .always : .onPoorGPS)
-                strongSelf.voiceController = CustomVoiceController(navigationService: navigationService)
-                let navigationOptions = NavigationOptions(navigationService: navigationService, voiceController: strongSelf.voiceController)
+                
+                // `MultiplexedSpeechSynthesizer` will provide "a backup" functionality to cover cases, which
+                // our custom implementation cannot handle.
+                let speechSynthesizer = MultiplexedSpeechSynthesizer([CustomVoiceController(), SystemSpeechSynthesizer()] as? [SpeechSynthesizing])
+                let routeController = RouteVoiceController(navigationService: navigationService, speechSynthesizer: speechSynthesizer)
+                // Remember to pass our `Voice Controller` to `Navigation Options`!
+                let navigationOptions = NavigationOptions(navigationService: navigationService, voiceController: routeController)
+                
                 let navigationViewController = NavigationViewController(for: route, routeOptions: routeOptions, navigationOptions: navigationOptions)
                 navigationViewController.modalPresentationStyle = .fullScreen
                 
@@ -37,38 +41,159 @@ class CustomVoiceControllerUI: UIViewController {
             }
         }
     }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        voiceController = nil
-    }
 }
 
-class CustomVoiceController: MapboxVoiceController {
+class CustomVoiceController: NSObject, SpeechSynthesizing {
     
-    // You will need audio files for as many or few cases as you'd like to handle
-    // This example just covers left, right and straight.
-    let turnLeft = NSDataAsset(name: "turnleft")!.data
-    let turnRight = NSDataAsset(name: "turnright")!.data
-    let straight = NSDataAsset(name: "continuestraight")!.data
+    // MARK: - SpeechSynthesizing implementation
     
-    override func didPassSpokenInstructionPoint(notification: NSNotification) {
-        let routeProgress = notification.userInfo![RouteController.NotificationUserInfoKey.routeProgressKey] as! RouteProgress
-        let soundForInstruction = audio(for: routeProgress.currentLegProgress.currentStep)
-        let instruction = notification.userInfo![RouteController.NotificationUserInfoKey.spokenInstructionKey] as! SpokenInstruction
-        play(instruction: instruction, data: soundForInstruction)
+    var delegate: SpeechSynthesizingDelegate?
+    
+    public var muted: Bool = false {
+        didSet {
+            updatePlayerVolume(audioPlayer)
+        }
+    }
+    public var volume: Float = 1.0 {
+        didSet {
+            audioPlayer?.volume = volume
+        }
+    }
+    public var isSpeaking: Bool {
+        return audioPlayer?.isPlaying ?? false
     }
     
-    func audio(for step: RouteStep) -> Data {
+    var locale: Locale?
+    
+    func prepareIncomingSpokenInstructions(_ instructions: [SpokenInstruction], locale: Locale?) {
+        // do nothing as we don't have to prepare anything
+    }
+    
+    func stopSpeaking() {
+        audioPlayer?.stop()
+    }
+    
+    func interruptSpeaking() {
+        audioPlayer?.stop()
+    }
+    
+    // You will need audio files for as many or few cases as you'd like to handle
+    // This example just covers left and right. All other cases will fail the Custom Voice Controller and force a backup System Speech to kick in
+    let turnLeft = NSDataAsset(name: "turnleft")!.data
+    let turnRight = NSDataAsset(name: "turnright")!.data
+    
+    public var audioPlayer: AVAudioPlayer?
+    private var previousInstruction: SpokenInstruction?
+    
+    func speak(_ instruction: SpokenInstruction, during legProgress: RouteLegProgress, locale: Locale? = nil) {
+
+        guard let soundForInstruction = audio(for: legProgress.currentStep) else {
+            // When `MultiplexedSpeechSynthesizer` receives an error from one of it's Speech Synthesizers,
+            // it requests the next on the list
+            delegate?.speechSynthesizer(self,
+                                        didSpeak: instruction,
+                                        with: SpeechError.noData(instruction: instruction,
+                                                                 options: SpeechOptions(text: instruction.text)))
+            return
+        }
+        speak(instruction: instruction, instructionData: soundForInstruction)
+    }
+    
+    func audio(for step: RouteStep) -> Data? {
         switch step.maneuverDirection {
         case .left:
             return turnLeft
         case .right:
             return turnRight
         default:
-            return straight
+            return nil // this will force report that Custom View Controller is unable to handle this case
         }
+    }
+    
+    // Method to play provided audio data with some edge cases handling
+    func speak(instruction: SpokenInstruction, instructionData: Data) {
+        
+        if let audioPlayer = audioPlayer {
+            if let previousInstruction = previousInstruction, audioPlayer.isPlaying {
+                delegate?.speechSynthesizer(self,
+                                            didInterrupt: previousInstruction,
+                                            with: instruction)
+            }
+            
+            deinitAudioPlayer()
+        }
+        
+        switch safeInitializeAudioPlayer(data: instructionData,
+                                         instruction: instruction) {
+        case .success(let player):
+            audioPlayer = player
+            previousInstruction = instruction
+            audioPlayer?.play()
+        case .failure(let error):
+            safeUnduckAudio(instruction: instruction)
+            delegate?.speechSynthesizer(self,
+                                        didSpeak: instruction,
+                                        with: error)
+        }
+    }
+    
+    // MARK: - Audio control methods
+    
+    func safeDuckAudio(instruction: SpokenInstruction?){
+        if let error = AVAudioSession.sharedInstance().tryDuckAudio() {
+            delegate?.speechSynthesizer(self,
+                                        encounteredError: SpeechError.unableToControlAudio(instruction: instruction,
+                                                                                           action: .duck,
+                                                                                           underlying: error))
+        }
+    }
+    
+    func safeUnduckAudio(instruction: SpokenInstruction?) {
+        if let error = AVAudioSession.sharedInstance().tryUnduckAudio() {
+            delegate?.speechSynthesizer(self,
+                                        encounteredError: SpeechError.unableToControlAudio(instruction: instruction,
+                                                                                           action: .unduck,
+                                                                                           underlying: error))
+        }
+    }
+    
+    func updatePlayerVolume(_ player: AVAudioPlayer?) {
+        player?.volume = muted ? 0.0 : volume
+    }
+    
+    func safeInitializeAudioPlayer(data: Data, instruction: SpokenInstruction) -> Result<AVAudioPlayer, MapboxNavigation.SpeechError> {
+        do {
+            let player = try AVAudioPlayer(data: data)
+            player.delegate = self
+            updatePlayerVolume(player)
+            
+            return .success(player)
+        } catch {
+            return .failure(SpeechError.unableToInitializePlayer(playerType: AVAudioPlayer.self,
+                                                                 instruction: instruction,
+                                                                 synthesizer: nil,
+                                                                 underlying: error))
+        }
+    }
+    
+    func deinitAudioPlayer() {
+        audioPlayer?.stop()
+        audioPlayer?.delegate = nil
     }
 }
 
+extension CustomVoiceController: AVAudioPlayerDelegate {
+    public func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        safeUnduckAudio(instruction: previousInstruction)
+        
+        guard let instruction = previousInstruction else {
+            assert(false, "Speech Synthesizer finished speaking 'nil' instruction")
+            return
+        }
+        
+        delegate?.speechSynthesizer(self,
+                                    didSpeak: instruction,
+                                    with: nil)
+    }
+}
 
--- Podfile
+++ Podfile
@@ -1,8 +1,8 @@
 platform :ios, '10.0'
 use_frameworks!
 
-pod 'MapboxCoreNavigation', :git => 'https://github.com/mapbox/mapbox-navigation-ios.git', :tag => 'v1.0.0-beta.1'
-pod 'MapboxNavigation', :git => 'https://github.com/mapbox/mapbox-navigation-ios.git', :tag => 'v1.0.0-beta.1'
+pod 'MapboxCoreNavigation', :git => 'https://github.com/mapbox/mapbox-navigation-ios.git', :commit => 'bb3b5bcee12edeb5c6422121186a7a1010e58d2a'
+pod 'MapboxNavigation', :git => 'https://github.com/mapbox/mapbox-navigation-ios.git', :commit => 'bb3b5bcee12edeb5c6422121186a7a1010e58d2a'
 
 target 'Navigation-Examples' do
 end
