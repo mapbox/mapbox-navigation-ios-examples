690185af13f603d65cea02ed5ea4a3d96166c729
udumft <viktor.kononov@mapbox.com>
2020-07-29 12:22:20 +0300

    vk-voice-example: updaed voice example with latest API changes

--- Navigation-Examples/Examples/Custom-Voice-Controller.swift
+++ Navigation-Examples/Examples/Custom-Voice-Controller.swift
@@ -43,49 +43,14 @@ class CustomVoiceControllerUI: UIViewController {
     }
 }
 
-class CustomVoiceController: NSObject, SpeechSynthesizing {
-    
-    // MARK: - SpeechSynthesizing implementation
-    
-    var delegate: SpeechSynthesizingDelegate?
-    
-    public var muted: Bool = false {
-        didSet {
-            updatePlayerVolume(audioPlayer)
-        }
-    }
-    public var volume: Float = 1.0 {
-        didSet {
-            audioPlayer?.volume = volume
-        }
-    }
-    public var isSpeaking: Bool {
-        return audioPlayer?.isPlaying ?? false
-    }
-    
-    var locale: Locale?
-    
-    func prepareIncomingSpokenInstructions(_ instructions: [SpokenInstruction], locale: Locale?) {
-        // do nothing as we don't have to prepare anything
-    }
-    
-    func stopSpeaking() {
-        audioPlayer?.stop()
-    }
-    
-    func interruptSpeaking() {
-        audioPlayer?.stop()
-    }
+class CustomVoiceController: MapboxSpeechSynthesizer {
     
     // You will need audio files for as many or few cases as you'd like to handle
     // This example just covers left and right. All other cases will fail the Custom Voice Controller and force a backup System Speech to kick in
     let turnLeft = NSDataAsset(name: "turnleft")!.data
     let turnRight = NSDataAsset(name: "turnright")!.data
     
-    public var audioPlayer: AVAudioPlayer?
-    private var previousInstruction: SpokenInstruction?
-    
-    func speak(_ instruction: SpokenInstruction, during legProgress: RouteLegProgress, locale: Locale? = nil) {
+    override func speak(_ instruction: SpokenInstruction, during legProgress: RouteLegProgress, locale: Locale? = nil) {
 
         guard let soundForInstruction = audio(for: legProgress.currentStep) else {
             // When `MultiplexedSpeechSynthesizer` receives an error from one of it's Speech Synthesizers,
@@ -96,7 +61,7 @@ class CustomVoiceController: NSObject, SpeechSynthesizing {
                                                                  options: SpeechOptions(text: instruction.text)))
             return
         }
-        speak(instruction: instruction, instructionData: soundForInstruction)
+        speak(instruction, data: soundForInstruction)
     }
     
     func audio(for step: RouteStep) -> Data? {
@@ -109,91 +74,4 @@ class CustomVoiceController: NSObject, SpeechSynthesizing {
             return nil // this will force report that Custom View Controller is unable to handle this case
         }
     }
-    
-    // Method to play provided audio data with some edge cases handling
-    func speak(instruction: SpokenInstruction, instructionData: Data) {
-        
-        if let audioPlayer = audioPlayer {
-            if let previousInstruction = previousInstruction, audioPlayer.isPlaying {
-                delegate?.speechSynthesizer(self,
-                                            didInterrupt: previousInstruction,
-                                            with: instruction)
-            }
-            
-            deinitAudioPlayer()
-        }
-        
-        switch safeInitializeAudioPlayer(data: instructionData,
-                                         instruction: instruction) {
-        case .success(let player):
-            audioPlayer = player
-            previousInstruction = instruction
-            audioPlayer?.play()
-        case .failure(let error):
-            safeUnduckAudio(instruction: instruction)
-            delegate?.speechSynthesizer(self,
-                                        didSpeak: instruction,
-                                        with: error)
-        }
-    }
-    
-    // MARK: - Audio control methods
-    
-    func safeDuckAudio(instruction: SpokenInstruction?){
-        if let error = AVAudioSession.sharedInstance().tryDuckAudio() {
-            delegate?.speechSynthesizer(self,
-                                        encounteredError: SpeechError.unableToControlAudio(instruction: instruction,
-                                                                                           action: .duck,
-                                                                                           underlying: error))
-        }
-    }
-    
-    func safeUnduckAudio(instruction: SpokenInstruction?) {
-        if let error = AVAudioSession.sharedInstance().tryUnduckAudio() {
-            delegate?.speechSynthesizer(self,
-                                        encounteredError: SpeechError.unableToControlAudio(instruction: instruction,
-                                                                                           action: .unduck,
-                                                                                           underlying: error))
-        }
-    }
-    
-    func updatePlayerVolume(_ player: AVAudioPlayer?) {
-        player?.volume = muted ? 0.0 : volume
-    }
-    
-    func safeInitializeAudioPlayer(data: Data, instruction: SpokenInstruction) -> Result<AVAudioPlayer, MapboxNavigation.SpeechError> {
-        do {
-            let player = try AVAudioPlayer(data: data)
-            player.delegate = self
-            updatePlayerVolume(player)
-            
-            return .success(player)
-        } catch {
-            return .failure(SpeechError.unableToInitializePlayer(playerType: AVAudioPlayer.self,
-                                                                 instruction: instruction,
-                                                                 synthesizer: nil,
-                                                                 underlying: error))
-        }
-    }
-    
-    func deinitAudioPlayer() {
-        audioPlayer?.stop()
-        audioPlayer?.delegate = nil
-    }
 }
-
-extension CustomVoiceController: AVAudioPlayerDelegate {
-    public func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        safeUnduckAudio(instruction: previousInstruction)
-        
-        guard let instruction = previousInstruction else {
-            assert(false, "Speech Synthesizer finished speaking 'nil' instruction")
-            return
-        }
-        
-        delegate?.speechSynthesizer(self,
-                                    didSpeak: instruction,
-                                    with: nil)
-    }
-}
-
