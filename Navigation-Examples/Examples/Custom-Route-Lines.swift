280fa84fcb3ef009fff5b8467d05bcc3f6474815
Maxim Makhun <maxim.makhun@mapbox.com>
2021-04-16 15:36:44 -0700

    Improve route line styling example.

--- Navigation-Examples/Examples/Custom-Route-Lines.swift
+++ Navigation-Examples/Examples/Custom-Route-Lines.swift
@@ -8,9 +8,35 @@ import Turf
 class CustomRouteLinesViewController: UIViewController {
     
     var navigationMapView: NavigationMapView!
-    var route: Route!
+    
     var navigationRouteOptions: NavigationRouteOptions!
-    var startNavigationButton: UIButton!
+    
+    var currentRoute: Route? {
+        get {
+            return routes?.first
+        }
+        set {
+            guard let selected = newValue else { routes = nil; return }
+            guard let routes = routes else { self.routes = [selected]; return }
+            self.routes = [selected] + routes.filter { $0 != selected }
+        }
+    }
+    
+    var routes: [Route]? {
+        didSet {
+            guard let routes = routes, let currentRoute = routes.first else {
+                navigationMapView.removeRoutes()
+                navigationMapView.removeWaypoints()
+                waypoints.removeAll()
+                return
+            }
+
+            navigationMapView.show(routes)
+            navigationMapView.showWaypoints(on: currentRoute)
+        }
+    }
+
+    var waypoints: [Waypoint] = []
     
     // MARK: - UIViewController lifecycle methods
     
@@ -18,16 +44,8 @@ class CustomRouteLinesViewController: UIViewController {
         super.viewDidLoad()
 
         setupNavigationMapView()
-        setupStartNavigationButton()
-        requestRoute()
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-
-        startNavigationButton.layer.cornerRadius = startNavigationButton.bounds.midY
-        startNavigationButton.clipsToBounds = true
-        startNavigationButton.setNeedsDisplay()
+        setupPerformActionBarButtonItem()
+        setupGestureRecognizers()
     }
     
     // MARK: - Setting-up methods
@@ -39,32 +57,65 @@ class CustomRouteLinesViewController: UIViewController {
         navigationMapView.mapView.update {
             $0.location.puckType = .puck2D()
         }
+        
+        let navigationViewportDataSource = NavigationViewportDataSource(navigationMapView.mapView, viewportDataSourceType: .raw)
+        navigationMapView.navigationCamera.viewportDataSource = navigationViewportDataSource
 
         view.addSubview(navigationMapView)
     }
     
-    func setupStartNavigationButton() {
-        startNavigationButton = UIButton()
-        startNavigationButton.setTitle("Start Navigation", for: .normal)
-        startNavigationButton.translatesAutoresizingMaskIntoConstraints = false
-        startNavigationButton.backgroundColor = .lightGray
-        startNavigationButton.setTitleColor(.darkGray, for: .highlighted)
-        startNavigationButton.setTitleColor(.white, for: .normal)
-        startNavigationButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
-        startNavigationButton.addTarget(self, action: #selector(startNavigationButtonPressed(_:)), for: .touchUpInside)
-        startNavigationButton.isHidden = true
-        view.addSubview(startNavigationButton)
-
-        startNavigationButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
-        startNavigationButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+    func setupPerformActionBarButtonItem() {
+        let settingsBarButtonItem = UIBarButtonItem(title: NSString(string: "\u{2699}\u{0000FE0E}") as String,
+                                                    style: .plain,
+                                                    target: self,
+                                                    action: #selector(performAction))
+        settingsBarButtonItem.setTitleTextAttributes([.font: UIFont.systemFont(ofSize: 30)], for: .normal)
+        settingsBarButtonItem.setTitleTextAttributes([.font: UIFont.systemFont(ofSize: 30)], for: .highlighted)
+        navigationItem.rightBarButtonItem = settingsBarButtonItem
+    }
+    
+    @objc func performAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "Perform action",
+                                                message: "Select specific action to perform it", preferredStyle: .actionSheet)
+        
+        typealias ActionHandler = (UIAlertAction) -> Void
+        
+        let startNavigation: ActionHandler = { _ in self.startNavigation() }
+        let removeRoutes: ActionHandler = { _ in self.routes = nil }
+        
+        let actions: [(String, UIAlertAction.Style, ActionHandler?)] = [
+            ("Start Navigation", .default, startNavigation),
+            ("Remove Routes", .default, removeRoutes),
+            ("Cancel", .cancel, nil)
+        ]
+        
+        actions
+            .map({ payload in UIAlertAction(title: payload.0, style: payload.1, handler: payload.2) })
+            .forEach(alertController.addAction(_:))
+        
+        if let popoverController = alertController.popoverPresentationController {
+            popoverController.barButtonItem = navigationItem.rightBarButtonItem
+        }
+        
+        present(alertController, animated: true, completion: nil)
     }
 
-    @objc func startNavigationButtonPressed(_ sender: UIButton) {
+    func setupGestureRecognizers() {
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
+        navigationMapView.addGestureRecognizer(longPressGestureRecognizer)
+    }
+    
+    @objc func startNavigation() {
+        guard let route = currentRoute else {
+            print("Please select at least one destination coordinate to start navigation.")
+            return
+        }
+        
         let navigationService = MapboxNavigationService(route: route,
                                                         routeIndex: 0,
                                                         routeOptions: navigationRouteOptions,
                                                         simulating: simulationIsEnabled ? .always : .onPoorGPS)
-
+        
         let navigationOptions = NavigationOptions(navigationService: navigationService)
         let navigationViewController = NavigationViewController(for: route,
                                                                 routeIndex: 0,
@@ -77,41 +128,63 @@ class CustomRouteLinesViewController: UIViewController {
 
         present(navigationViewController, animated: true, completion: nil)
     }
+    
+    @objc func handleLongPress(_ gesture: UILongPressGestureRecognizer) {
+        guard gesture.state == .began else { return }
 
-    func requestRoute() {
-        let origin = CLLocationCoordinate2DMake(37.773, -122.411)
-        let destination = CLLocationCoordinate2DMake(37.763252389415186, -122.40061448679577)
-        let navigationRouteOptions = NavigationRouteOptions(coordinates: [origin, destination])
+        createWaypoints(for: navigationMapView.mapView.coordinate(for: gesture.location(in: navigationMapView.mapView)))
+        requestRoute()
+    }
+
+    func createWaypoints(for destinationCoordinate: CLLocationCoordinate2D?) {
+        guard let destinationCoordinate = destinationCoordinate else { return }
+        guard let userLocation = navigationMapView.mapView.locationManager.latestLocation?.internalLocation else {
+            print("User location is not valid. Make sure to enable Location Services.")
+            return
+        }
         
         let cameraOptions = CameraOptions(center: origin, zoom: 13.0)
         self.navigationMapView.mapView.camera.setCamera(to: cameraOptions)
         
+        // Add destination waypoint to list of waypoints.
+        let waypoint = Waypoint(coordinate: destinationCoordinate)
+        waypoint.targetCoordinate = destinationCoordinate
+        waypoints.append(waypoint)
+    }
+
+    func requestRoute() {
+        let navigationRouteOptions = NavigationRouteOptions(waypoints: waypoints)
         Directions.shared.calculate(navigationRouteOptions) { [weak self] (session, result) in
             switch result {
             case .failure(let error):
-                print(error.localizedDescription)
+                print("Error occured while requesting route: \(error.localizedDescription).")
+
+                // In case if direction calculation failed - remove last destination waypoint.
+                self?.waypoints.removeLast()
             case .success(let response):
-                guard let routes = response.routes,
-                      let currentRoute = routes.first,
-                      let self = self else { return }
-                
-                self.route = currentRoute
-                self.navigationRouteOptions = navigationRouteOptions
-                self.startNavigationButton.isHidden = false
-                self.navigationMapView.show(routes)
-                self.navigationMapView.showWaypoints(on: currentRoute)
+                guard let routes = response.routes else { return }
+                self?.navigationRouteOptions = navigationRouteOptions
+                self?.routes = routes
+                self?.navigationMapView.show(routes)
+                if let currentRoute = self?.currentRoute {
+                    self?.navigationMapView.showWaypoints(on: currentRoute)
+                }
+
+                if let coordinates = self?.waypoints.compactMap({ $0.targetCoordinate }) {
+                    self?.navigationMapView.highlightBuildings(at: coordinates, in3D: true)
+                }
             }
         }
-    }
-    
-    func navigationViewControllerDidDismiss(_ navigationViewController: NavigationViewController, byCanceling canceled: Bool) {
-        dismiss(animated: true, completion: nil)
     }
 }
 
 // MARK: - NavigationMapViewDelegate methods
 
 extension CustomRouteLinesViewController: NavigationMapViewDelegate {
+    
+    func navigationMapView(_ mapView: NavigationMapView, didSelect route: Route) {
+        self.currentRoute = route
+    }
     
     func navigationMapView(_ navigationMapView: NavigationMapView, shapeFor route: Route) -> LineString? {
         return route.shape
@@ -147,6 +220,10 @@ extension CustomRouteLinesViewController: NavigationMapViewDelegate {
 // MARK: - NavigationViewControllerDelegate methods
 
 extension CustomRouteLinesViewController: NavigationViewControllerDelegate {
+    
+    func navigationViewControllerDidDismiss(_ navigationViewController: NavigationViewController, byCanceling canceled: Bool) {
+        dismiss(animated: true, completion: nil)
+    }
     
     func navigationViewController(_ navigationViewController: NavigationViewController, routeLineLayerWithIdentifier identifier: String, sourceIdentifier: String) -> LineLayer? {
         var lineLayer = LineLayer(id: identifier)
