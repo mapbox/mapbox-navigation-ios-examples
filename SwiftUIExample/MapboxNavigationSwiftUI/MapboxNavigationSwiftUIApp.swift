49b3b658a6782b24edeb0d875a408d0eaaabdbfa
Aliaksandr Bialiauski <aliaksandr.bialiauski@mapbox.com>
2022-08-04 14:46:58 +0300

    Add SwiftUI example

---
+++ SwiftUIExample/MapboxNavigationSwiftUI/MapboxNavigationSwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  MapboxNavigationSwiftUIApp.swift
+//  MapboxNavigationSwiftUI
+//
+//  Created by Aliaksandr Bialiauski on 16.06.22.
+//
+
+import SwiftUI
+
+
+@main
+struct MapboxNavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
