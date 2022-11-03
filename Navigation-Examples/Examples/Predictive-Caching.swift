0792f2639c390174442fc4d414d734c1d1984d78
Nastassia Makaranka <nastassia.makaranka@mapbox.com>
2022-11-03 12:12:03 +0100

    Add predictive cache configuration to Nav SDK docs

--- Navigation-Examples/Examples/Predictive-Caching.swift
+++ Navigation-Examples/Examples/Predictive-Caching.swift
@@ -38,12 +38,18 @@ class PredictiveCachingViewController: UIViewController {
                 
                 // When predictive caching is enabled, the Navigation SDK will create a cache of data within three configurable boundaries.
                 var predictiveCacheOptions = PredictiveCacheOptions()
+                // Predictive cache should be configured separately for navigation and maps
                 // Radius around the user's location. Defaults to 2000 meters.
-                predictiveCacheOptions.routeBufferRadius = 300
+                predictiveCacheOptions.predictiveCacheNavigationOptions.locationOptions.routeBufferRadius = 300
                 // Buffer around the route. Defaults to 500 meters.
-                predictiveCacheOptions.currentLocationRadius = 2000
+                predictiveCacheOptions.predictiveCacheNavigationOptions.locationOptions.currentLocationRadius = 2000
                 // Radius around the destination. Defaults to 5000 meters.
-                predictiveCacheOptions.destinationLocationRadius = 3000
+                predictiveCacheOptions.predictiveCacheNavigationOptions.locationOptions.destinationLocationRadius = 3000
+
+                // You can specify zoom range for map data caching.
+                predictiveCacheOptions.predictiveCacheMapsOptions.zoomRange = 5...12
+                // Location cache properties can also be configured for map data caching.
+                predictiveCacheOptions.predictiveCacheMapsOptions.locationOptions.destinationLocationRadius = 2000
                 
                 let navigationOptions = NavigationOptions(navigationService: navigationService,
                                                           predictiveCacheOptions: predictiveCacheOptions)
